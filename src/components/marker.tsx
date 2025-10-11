06c37735b1a0a9de0ffccd147aed89038c47ff62
Konrad Guzek <konrad@guzek.uk>
2025-10-11 20:20:43 +0200

    feat: marker tooltips

--- src/components/marker.tsx
+++ src/components/marker.tsx
@@ -1,30 +1,89 @@
-import type { ReactNode } from "react";
-
 import { LAYER_ICONS } from "@/config/icons";
 import { Layer } from "@/lib/enums";
+import { cn } from "@/lib/utils";
 import type {
   IconElementType,
   LayerLocation,
   LayerMetadata,
 } from "@/types/app";
+import type { SynchronousReactNode } from "@/types/helpers";
 
-const LAYER_FORMATTERS: Partial<{
-  [L in Layer]: (meta: LayerMetadata[L]) => ReactNode;
-}> = {
-  [Layer.Smog]: (meta) => <div>Jakość powietrza: {meta.airQuality.value}</div>,
-  [Layer.Fires]: (meta) => <div>Wielkość pożaru: {meta.intensity}</div>,
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+
+const LAYER_FORMATTERS: {
+  [L in Layer]: (
+    icon: IconElementType,
+    meta: LayerMetadata[L],
+  ) => {
+    marker: SynchronousReactNode;
+    tooltip: SynchronousReactNode;
+  };
+} = {
+  [Layer.Smog]: (Icon, meta) => ({
+    marker: (
+      <div>
+        <Icon
+          className={cn(
+            // TODO: zmień kolory/rozmiary
+            meta.airQuality.overallValue < 1
+              ? "text-green-500"
+              : meta.airQuality.overallValue < 2
+                ? "text-yellow-500"
+                : meta.airQuality.overallValue < 3
+                  ? "text-orange-500"
+                  : meta.airQuality.overallValue < 4
+                    ? "text-red-500"
+                    : "text-neutral-500",
+          )}
+        />
+      </div>
+    ),
+    tooltip: <div>Jakość powietrza: {meta.airQuality.overallCategoryName}</div>,
+  }),
+  [Layer.Fires]: (Icon, meta) => ({
+    marker: (
+      <div>
+        <Icon /> Wielkość pożaru: {meta.intensity}
+      </div>
+    ),
+    tooltip: null,
+  }),
+  [Layer.Floods]: (Icon) => ({
+    marker: (
+      <div>
+        <Icon /> Powódź
+      </div>
+    ),
+    tooltip: null,
+  }),
+  [Layer.Shelters]: (Icon) => ({
+    marker: (
+      <div>
+        <Icon /> Schron
+      </div>
+    ),
+    tooltip: null,
+  }),
+  [Layer.AEDs]: (Icon) => ({
+    marker: (
+      <div>
+        <Icon /> Defibrylator
+      </div>
+    ),
+    tooltip: null,
+  }),
 };
 
 export function Marker<T extends Layer>({
   layer,
   meta,
 }: { layer: T } & LayerLocation<T>) {
-  const Icon: IconElementType = LAYER_ICONS[layer];
   const formatter = LAYER_FORMATTERS[layer];
+  const { marker, tooltip } = formatter(LAYER_ICONS[layer], meta);
   return (
-    <div className="z-[200]">
-      <Icon />
-      {formatter?.(meta)}
-    </div>
+    <Tooltip>
+      <TooltipTrigger>{marker}</TooltipTrigger>
+      <TooltipContent>{tooltip}</TooltipContent>
+    </Tooltip>
   );
 }
