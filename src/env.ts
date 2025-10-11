44bf3dbaf0f3ec68a00e745514b4dacd6a778434
Kamil Marczak <kamilm@you2.pl>
2025-10-11 23:29:29 +0200

    fix: db url

--- src/env.ts
+++ src/env.ts
@@ -11,7 +11,7 @@ try {
 export const env = createEnv({
   server: {
     NASA_FIRMS_MAP_KEY: z.string().length(32),
-    DATABASE_URL: z.string().min(1),
+    DATABASE_URI: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_GOOGLE_MAPS_KEY: z.string().length(39),
@@ -19,7 +19,7 @@ export const env = createEnv({
   runtimeEnv: {
     NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
     NASA_FIRMS_MAP_KEY: process.env.NASA_FIRMS_MAP_KEY,
-    DATABASE_URL: process.env.DATABASE_URL,
+    DATABASE_URI: process.env.DATABASE_URI,
   },
   skipValidation: process.env.SKIP_ENV_VALIDATION === "true",
 });
