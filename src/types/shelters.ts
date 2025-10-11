831e89141a151af04fe7c426d229f2e6f4c2dc18
Kamil Marczak <kamilm@you2.pl>
2025-10-11 19:17:03 +0200

    feat: shelter get fn

---
+++ src/types/shelters.ts
@@ -0,0 +1,52 @@
+export interface SheltersResponse {
+  objectIdFieldName: string;
+  uniqueIdField: {
+    name: string;
+    isSystemMaintained: boolean;
+  };
+  globalIdFieldName: string;
+  geometryType: string;
+  spatialReference: {
+    wkid: number;
+    latestWkid: number;
+  };
+  fields: unknown[]; // Pole fields jest puste w przykładzie, więc używam unknown[]
+  features: ShelterFeature[];
+}
+
+export interface ShelterFeature {
+  attributes: ShelterAttributes;
+  geometry: {
+    x: number;
+    y: number;
+  };
+}
+
+export interface ShelterAttributes {
+  ObjectID: number;
+  Rodzaj_inw: string;
+  Możliwoś: string;
+  Powierzchn: number;
+  Pojemnoś_: number;
+  Subiektywn: number;
+  Rodzaj_obi: string;
+  Przeznacze: string;
+  Województ: string;
+  Powiat: string;
+  Adres: string;
+  x: number;
+  y: number;
+  ObjectId2: number;
+  Gmina: string | null;
+  Miejscowosc: string | null;
+}
+
+export interface SheltersData {
+  id: number;
+  address: string;
+  capacity: string;
+  area: string;
+  type: string;
+  buildingType: string;
+  purpose: string;
+}
