a98b0308cb5266777bc8fbe546a096a053ec7417
Konrad Guzek <konrad@guzek.uk>
2025-10-11 18:17:59 +0200

    feat: fetch locations based on map center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,5 @@
 import { LayersMap } from "@/components/map";
-import { Layer } from "@/lib/enums";
-import { getAirQuality } from "@/lib/services/smog";
 
-export default async function Home() {
-  const [
-    airQuality,
-    // ...
-    // eslint-disable-next-line unicorn/no-single-promise-in-promise-methods
-  ] = await Promise.all([
-    getAirQuality(),
-    // ...
-  ]);
-  return (
-    <LayersMap
-      locations={{
-        [Layer.Smog]: airQuality,
-        [Layer.Fires]: [],
-        [Layer.Floods]: [],
-        [Layer.Shelters]: [],
-        [Layer.AEDs]: [],
-      }}
-    />
-  );
+export default function Home() {
+  return <LayersMap />;
 }
---
+++ src/lib/map-provider.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import type { ReactNode } from "react";
+
+import { LAYER_FETCH_FUNCTIONS, SKS_COORDINATES } from "@/config/constants";
+import { MapContext } from "@/hooks/use-map";
+
+import { Layer } from "./enums";
+import { typedEntries } from "./helpers/typescript";
+
+export function MapContextProvider({ children }: { children: ReactNode }) {
+  const [center, setCenter] = useState(SKS_COORDINATES);
+  const [zoom, setZoom] = useState(2);
+  const [enabledLayers, setEnabledLayers] = useState<Record<Layer, boolean>>({
+    [Layer.Smog]: true,
+    [Layer.Fires]: false,
+    [Layer.Floods]: false,
+    [Layer.Shelters]: false,
+    [Layer.AEDs]: false,
+  });
+
+  async function fetchLocations() {
+    const layersToFetch = typedEntries(enabledLayers).filter(
+      ([_layer, enabled]) => enabled,
+    );
+    const locations = await Promise.all(
+      layersToFetch.map(async ([layer]) => {
+        const fetchFunction = LAYER_FETCH_FUNCTIONS[layer];
+        const result = await fetchFunction(center);
+        return result.map((location) => ({ ...location, layer }));
+      }),
+    );
+    return locations.flat();
+  }
+
+  const query = useQuery({
+    queryKey: ["map", "locations", center, enabledLayers],
+    queryFn: fetchLocations,
+    refetchOnMount: true,
+  });
+
+  function toggleLayer(layer: Layer) {
+    setEnabledLayers((previous) => ({
+      ...previous,
+      [layer]: !previous[layer],
+    }));
+  }
+
+  return (
+    <MapContext.Provider
+      value={{
+        enabledLayers,
+        toggleLayer,
+        center,
+        setCenter,
+        zoom,
+        setZoom,
+        locations: query.data ?? [],
+      }}
+    >
+      {children}
+    </MapContext.Provider>
+  );
+}
