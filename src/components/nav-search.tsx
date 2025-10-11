97281e34abc0fc01ea31c0375e3464f5a62f6c24
Kamil Marczak <79667721+qamarq@users.noreply.github.com>
2025-10-12 01:33:54 +0200

    feat: search cities (#13)
    Co-authored-by: Konrad Guzek <konrad@guzek.uk>

---
+++ src/components/nav-search.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Compass, Search } from "lucide-react";
+import React, { useState } from "react";
+import { geocodeByAddress, getLatLng } from "react-google-places-autocomplete";
+
+import { useMap } from "@/hooks/use-map";
+
+import { Button } from "./ui/button";
+import { ButtonGroup } from "./ui/button-group";
+import { InputGroup, InputGroupAddon, InputGroupInput } from "./ui/input-group";
+
+const getCoordsFromAddress = async (address: string) => {
+  try {
+    const results = await geocodeByAddress(address);
+    const { lat, lng } = await getLatLng(results[0]);
+    return { lat, lng };
+  } catch (error) {
+    console.error("Error getting coordinates:", error);
+    return null;
+  }
+};
+
+export function NavSearch() {
+  const [inputValue, setInputValue] = useState("");
+  const { setCenter, setZoom } = useMap();
+
+  const handleSearch = async () => {
+    if (inputValue.trim()) {
+      const coords = await getCoordsFromAddress(inputValue);
+      if (coords !== null) {
+        setCenter({ lat: coords.lat, lng: coords.lng });
+        setZoom(14);
+      }
+    }
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      void handleSearch();
+    }
+  };
+
+  return (
+    <ButtonGroup className="w-full [--radius:9999rem]">
+      <ButtonGroup className="w-full">
+        <InputGroup className="bg-white">
+          <InputGroupAddon>
+            <Search />
+          </InputGroupAddon>
+          <InputGroupInput
+            value={inputValue}
+            onChange={(event) => {
+              setInputValue(event.target.value);
+            }}
+            onKeyDown={handleKeyDown}
+            placeholder={"Dokąd się wybierasz?"}
+          />
+        </InputGroup>
+      </ButtonGroup>
+      <ButtonGroup>
+        <Button variant="outline" size="icon">
+          <Compass />
+        </Button>
+      </ButtonGroup>
+    </ButtonGroup>
+  );
+}
