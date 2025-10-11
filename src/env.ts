00a024e09452393f8b9632648d73612db92fba85
Konrad Guzek <konrad@guzek.uk>
2025-10-11 21:17:49 +0200

    chore: move keys from client to server

--- src/env.ts
+++ src/env.ts
@@ -2,12 +2,15 @@ import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
 export const env = createEnv({
-  server: {},
+  server: {
+    NASA_FIRMS_MAP_KEY: z.string().length(32),
+  },
   client: {
     NEXT_PUBLIC_GOOGLE_MAPS_KEY: z.string().length(39),
   },
   runtimeEnv: {
     NEXT_PUBLIC_GOOGLE_MAPS_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_KEY,
+    NASA_FIRMS_MAP_KEY: process.env.NASA_FIRMS_MAP_KEY,
   },
   skipValidation: process.env.SKIP_ENV_VALIDATION === "true",
 });
