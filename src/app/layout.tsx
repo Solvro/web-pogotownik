50a494a25cf399aacf05f75d01427a5e045ce533
michalges <michal.ges.mail@gmail.com>
2025-12-23 01:35:49 +0100

    feat(metadata): new og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,13 +25,13 @@ export const metadata: Metadata = {
   openGraph: {
     url: PRODUCTION_URL,
     images: {
-      url: "/hero.png",
-      width: 2880,
-      height: 1800,
+      url: "/og-image.png",
+      width: 2560,
+      height: 1842,
     },
   },
   twitter: {
-    images: ["/hero.png"],
+    images: ["/og-image.png"],
   },
 };
 
